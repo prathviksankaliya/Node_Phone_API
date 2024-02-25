7dd048de1c28eb148905f51e9c1ba3723a7f2610
Shadow <prathviksankaliya@gmail.com>
2024-02-25 14:18:50 +0530

    Phone API Updated..

--- app.js
+++ app.js
@@ -13,28 +13,32 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Static phone numbers (plz enter verified phone numbers)
-const from = process.env.TWILIO_PHONE_NUMBER; // Twilio phone number
-const to = process.env.PRATHVIK_PHONE_NUMBER; // The number you want to call
-
+let caller = [
+    process.env.DARSH_PHONE_NUMBER,
+    // process.env.C1_PHONE_NUMBER,
+    // process.env.R1_PHONE_NUMBER,
+    process.env.B1_PHONE_NUMBER
+]
 // POST endpoint to receive JSON data
-app.post('/', (req, res) => {
+
+const callerHandler = async (credentials) => {
+    if(credentials.length==0)
+        return {success:"calling done"};
+    await client.calls.create({
+        twiml: `<Response><Say>ALERT GOLD ALERT GOLD ALERT GOLD ALERT GOLD ALERT GOLD ALERT GOLD ALERT GOLD ALERT GOLDALERT GOLD ALERT GOLDALERT GOLD ALERT GOLD ALERT GOLD ALERT GOLD ALERT GOLD ALERT GOLD</Say></Response>`,
+        to:credentials[credentials.length-1],
+        from:process.env.TWILIO_PHONE_NUMBER
+    });
+    return callerHandler(credentials.slice(0,credentials.length-1))
+}
+app.post('/', async (req, res) => {
     const jsonData = req.body;
     if (jsonData.phone === 'true') {
-        client.calls
-            .create({
-                twiml: `<Response><Say>HELLO</Say></Response>`,
-                to,
-                from,
-            })
-            .then(call => {
-                res.json({ message: 'Call initiated successfully', callSid: call.sid });
-            })
-            .catch(err => {
-                res.json({ error: 'Failed to initiate call' });
-            });
+        res.json(await callerHandler(caller));
     } else {
         res.json({ message: 'No call initiated' });
     }
+    res.end()
 });
 
 const port = process.env.PORT;
